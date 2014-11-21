769441376c477116cdb1bb59b0f6011dcee4a8cd
Auster Chen <auster.s.chen@gmail.com>
2014-11-20 16:15:16 -0800

    fixed up entries

--- app/assets/javascripts/models/entry.js
+++ app/assets/javascripts/models/entry.js
@@ -1,3 +1,23 @@
 NewsReader.Models.Entry = Backbone.Model.extend({
-    urlRoot: "/api/entries"
+    urlRoot: "/api/entries",
+    
+    description: function(){
+        return this._description;
+    },
+    link: function(){
+        return this._link;
+    },
+    date: function(){
+        return this._date;
+    },
+    parse: function(JSONresp){
+        var jsonText = JSONresp.json;
+        var json = JSON.parse(jsonText);
+        this._description = json.description;
+        this._link = json.link;
+        this._date = json.pubDate;
+        
+        delete JSONresp.json
+        return JSONresp;
+    }
 })
